d69fae394d71aa00fa179e43f463fbbdb8e0af1b
jimcky <jimcky.ok@163.com>
2017-08-07 20:30:55 +0800

    add file

---
+++ SmartApartment/Controller/Home/View/HotelSelectView.h
@@ -0,0 +1,31 @@
+//
+//  HotelSelectView.h
+//  SmartApartment
+//
+//  Created by jimcky on 2017/8/7.
+//  Copyright © 2017年 Jimcky Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSInteger, HotelSelectBtnType) {
+    
+    HotelSelectBtnTypeAlldayRoom,   // 全日房
+    HotelSelectBtnTypeHoursRoom,    // 钟点房
+    HotelSelectBtnTypeCitySelect,   // 城市选择
+    HotelSelectBtnTypeLocate,       // 定位
+    HotelSelectBtnTypeLiveDate,     // 入住时间
+    HotelSelectBtnTypeLeaveDate,    // 离店时间
+    HotelSelectBtnTypeSearchHotel,  // 查找酒店
+};
+
+@protocol HotelSelectView <NSObject>
+
+- (void)hotelSelectViewDidClickBtn:(HotelSelectBtnType)type;
+
+@end
+
+
+@interface HotelSelectView : UIView
+
+@end
---
+++ SmartApartment/Controller/Home/View/HotelSelectView.m
@@ -0,0 +1,23 @@
+//
+//  HotelSelectView.m
+//  SmartApartment
+//
+//  Created by jimcky on 2017/8/7.
+//  Copyright © 2017年 Jimcky Lin. All rights reserved.
+//
+
+#import "HotelSelectView.h"
+
+@implementation HotelSelectView
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+    }
+    return self;
+}
+
+
+@end
