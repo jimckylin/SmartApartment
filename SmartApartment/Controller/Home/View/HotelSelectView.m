5076e69f253a9ca9ebd3caa675e0f747a4fa4f88
jimcky <jimcky.ok@163.com>
2017-08-08 15:06:00 +0800

    home view

--- SmartApartment/Controller/Home/View/HotelSelectView.m
+++ SmartApartment/Controller/Home/View/HotelSelectView.m
@@ -19,6 +19,14 @@
 @property (nonatomic, strong) UIButton *liveBtn;
 @property (nonatomic, strong) UIButton *leaveBtn;
 
+@property (nonatomic, strong) UILabel *liveLabel;
+@property (nonatomic, strong) UILabel *leaveLabel;
+@property (nonatomic, strong) UILabel *countLabel;
+@property (nonatomic, strong) UILabel *extraLabel;
+@property (nonatomic, strong) UIImageView *arrowIconV;
+
+@property (nonatomic, strong) UIButton *searchBtn;
+
 @end
 
 @implementation HotelSelectView
@@ -101,7 +109,7 @@
     [selectView addSubview:_locateBtn];
     
     UIView *line1 = [[UIView alloc] init];
-    line1.frame = CGRectMake(30, 50, kScreenWidth - 60, 1);
+    line1.frame = CGRectMake(30, 50, kScreenWidth - 60, 0.5);
     line1.backgroundColor = [UIColor lightGrayColor];
     [selectView addSubview:line1];
     
@@ -123,16 +131,67 @@
     [_leaveBtn setTitleColor:[UIColor darkTextColor] forState:UIControlStateSelected];
     [_leaveBtn setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
     [_leaveBtn.titleLabel setFont:[UIFont systemFontOfSize:13]];
-    _leaveBtn.titleLabel.textAlignment = NSTextAlignmentRight; // 文字在titleLabel中右对齐(并没有看出有什么卵用)
-    [_leaveBtn setContentHorizontalAlignment:(UIControlContentHorizontalAlignmentRight)];
     _leaveBtn.imageEdgeInsets = UIEdgeInsetsMake(0, 0, 0, 20);
     _leaveBtn.tag = HotelSelectBtnTypeLeaveDate;
     [selectView addSubview:_leaveBtn];
     
+    _liveLabel = [UILabel new];
+    _liveLabel.font = [UIFont systemFontOfSize:10];
+    _liveLabel.textColor = [UIColor lightGrayColor];
+    _liveLabel.textAlignment = NSTextAlignmentCenter;
+    _liveLabel.numberOfLines = 0;
+    _liveLabel.text = @"入住\n周二";
+    [selectView addSubview:_liveLabel];
+    
+    _leaveLabel = [UILabel new];
+    _leaveLabel.font = [UIFont systemFontOfSize:10];
+    _leaveLabel.textColor = [UIColor lightGrayColor];
+    _leaveLabel.textAlignment = NSTextAlignmentCenter;
+    _leaveLabel.numberOfLines = 0;
+    _leaveLabel.text = @"离店\n周三";
+    [selectView addSubview:_leaveLabel];
+    
+    _countLabel = [UILabel new];
+    _countLabel.font = [UIFont systemFontOfSize:9];
+    _countLabel.textColor = [UIColor lightGrayColor];
+    _countLabel.textAlignment = NSTextAlignmentCenter;
+    _countLabel.layer.borderColor = [UIColor grayColor].CGColor;
+    _countLabel.layer.borderWidth = 0.5;
+    _countLabel.layer.cornerRadius = 15/2.;
+    _countLabel.text = @"共2晚";
+    [selectView addSubview:_countLabel];
+    
     UIView *line2 = [[UIView alloc] init];
-    line2.frame = CGRectMake(30, 100, kScreenWidth - 60, 1);
+    line2.frame = CGRectMake(30, 100, kScreenWidth - 60, 0.5);
     line2.backgroundColor = [UIColor lightGrayColor];
     [selectView addSubview:line2];
+    
+    _extraLabel = [UILabel new];
+    _extraLabel.font = [UIFont systemFontOfSize:13];
+    _extraLabel.textColor = [UIColor lightGrayColor];
+    _extraLabel.text = @"位置/品牌/酒店名称";
+    [selectView addSubview:_extraLabel];
+    
+    _arrowIconV = [UIImageView new];
+    _arrowIconV.image = kImage(@"home_arrow_iconiphone");
+    [selectView addSubview:_arrowIconV];
+    
+    UIView *line3 = [[UIView alloc] init];
+    line3.frame = CGRectMake(30, 100 + 40, kScreenWidth - 60, 0.5);
+    line3.backgroundColor = [UIColor lightGrayColor];
+    [selectView addSubview:line3];
+    
+    // 查找酒店
+    _searchBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    _searchBtn.backgroundColor = [UIColor redColor];
+    [_searchBtn addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
+    [_searchBtn setTitle:@"查找酒店" forState:UIControlStateNormal];
+    [_searchBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [_searchBtn setTitleColor:[UIColor lightGrayColor] forState:UIControlStateHighlighted];
+    [_searchBtn.titleLabel setFont:[UIFont systemFontOfSize:14]];
+    _searchBtn.tag = HotelSelectBtnTypeSearchHotel;
+    _searchBtn.layer.cornerRadius = 3;
+    [selectView addSubview:_searchBtn];
 }
 
 
@@ -188,11 +247,36 @@
     
     [_liveBtn autoPinEdgeToSuperviewEdge:ALEdgeLeft withInset:paddingX];
     [_liveBtn autoPinEdge:ALEdgeTop toEdge:ALEdgeBottom ofView:_cityBtn];
-    [_liveBtn autoSetDimensionsToSize:CGSizeMake(80, 50)];
+    [_liveBtn autoSetDimensionsToSize:CGSizeMake(65, 50)];
     
     [_leaveBtn autoPinEdgeToSuperviewEdge:ALEdgeRight withInset:paddingX + 30];
     [_leaveBtn autoPinEdge:ALEdgeTop toEdge:ALEdgeBottom ofView:_cityBtn];
-    [_leaveBtn autoSetDimensionsToSize:CGSizeMake(80, 50)];
+    [_leaveBtn autoSetDimensionsToSize:CGSizeMake(65, 50)];
+    
+    [_liveLabel autoPinEdge:ALEdgeLeft toEdge:ALEdgeRight ofView:_liveBtn];
+    [_liveLabel autoPinEdge:ALEdgeTop toEdge:ALEdgeBottom ofView:_cityBtn];
+    [_liveLabel autoSetDimensionsToSize:CGSizeMake(30, 50)];
+    
+    [_leaveLabel autoPinEdgeToSuperviewEdge:ALEdgeRight withInset:paddingX];
+    [_leaveLabel autoPinEdge:ALEdgeTop toEdge:ALEdgeBottom ofView:_cityBtn];
+    [_leaveLabel autoSetDimensionsToSize:CGSizeMake(30, 50)];
+    
+    [_countLabel autoAlignAxisToSuperviewAxis:ALAxisVertical];
+    [_countLabel autoAlignAxis:ALAxisHorizontal toSameAxisOfView:_liveLabel];
+    [_countLabel autoSetDimensionsToSize:CGSizeMake(40, 15)];
+    
+    [_extraLabel autoPinEdgeToSuperviewEdge:ALEdgeLeft withInset:paddingX];
+    [_extraLabel autoPinEdge:ALEdgeTop toEdge:ALEdgeBottom ofView:_liveLabel];
+    [_extraLabel autoPinEdgeToSuperviewEdge:ALEdgeRight withInset:paddingX+30];
+    [_extraLabel autoSetDimension:ALDimensionHeight toSize:40];
+    
+    [_arrowIconV autoPinEdgeToSuperviewEdge:ALEdgeRight withInset:paddingX];
+    [_arrowIconV autoAlignAxis:ALAxisHorizontal toSameAxisOfView:_extraLabel];
+    //[_countLabel autoSetDimensionsToSize:CGSizeMake(40, 15)];
+    
+    [_searchBtn autoAlignAxisToSuperviewAxis:ALAxisVertical];
+    [_searchBtn autoPinEdge:ALEdgeTop toEdge:ALEdgeBottom ofView:_extraLabel withOffset:20];
+    [_searchBtn autoSetDimensionsToSize:CGSizeMake(kScreenWidth-80, 40)];
     
     [super updateConstraints];
 }
